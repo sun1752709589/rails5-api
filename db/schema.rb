0c5eacf3f299b4da8850a3d3251d1b749d21c4ab
sun <1752709589@qq.com>
2016-07-01 14:26:44 +0800

    rails5-api on pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20160602174115) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
     t.boolean  "sex"
